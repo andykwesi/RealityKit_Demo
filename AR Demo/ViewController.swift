3de35ef7da9f6e2db22f7bf71e382d37c45e70a0
Andy Kwesi <36931378+andykwesi@users.noreply.github.com>
2022-03-30 22:24:36 +0000

    Add files via upload

---
+++ AR Demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR Demo
+//
+//  Created by Andy Kwesi on 3/24/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
